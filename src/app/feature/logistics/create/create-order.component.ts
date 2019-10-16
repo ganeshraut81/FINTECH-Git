652047a63f40c3bbbde645b9ed90c03c45b7c63b
ruchira2019 <ruchira2019@users.noreply.github.com>
2019-10-16 15:12:03 +0530

    added backward flow for stepper

--- src/app/feature/logistics/create/create-order.component.ts
+++ src/app/feature/logistics/create/create-order.component.ts
@@ -33,30 +33,38 @@ export class CreateOrderComponent implements OnInit {
     $(document).ready(function(){
     
       $(".next_btn").click(function() { // Function Runs On NEXT Button Click
-      $(this).parent().next().fadeIn('slow');
-      $(this).parent().css({
-      'display': 'none'
-      });
-      // Adding Class Active To Show Steps Forward;
-      $('.active').next(".stepicon").addClass('active');
-      $('.active').prev(".stepicon").addClass('traversed');
-     
-      });
-      $(".pre_btn").click(function() { // Function Runs On PREVIOUS Button Click
-          $(this).parent().prev().fadeIn('slow');
-          $(this).parent().css({
-          'display': 'none'
-          });
-          // Removing Class Active To Show Steps Backward;
-          // if( $(".stepicon:last").hasClass('active')){
-          // {
-          //     $(".stepicon:last").removeClass('active');
-          //   }
-          // }
-          // $('.active').prev(".traversed:last").removeClass('traversed');
-
-    
-      });
+        $(this).parent().next().fadeIn('slow');
+        $(this).parent().css({
+        'display': 'none'
+        });
+        // Adding Class Active To Show Steps Forward;
+        $('.active').next(".stepicon").addClass('active');
+        $('.active').prev(".stepicon").addClass('traversed');
+       
+        });
+      
+        $(".pre_btn").click(function() { 
+          // Function Runs On PREVIOUS Button Click
+            $(this).parent().prev().fadeIn('slow');
+            $(this).parent().css({
+            'display': 'none'
+            });
+        });
+        //removing active and traversed for backward flow
+        $(".prevbtn3").click(function() {
+          $(".step4").removeClass("active");
+          $(".step3").removeClass("traversed");
+        });
+  
+        $(".prevbtn2").click(function() {
+          $(".step3").removeClass("active");
+          $(".step2").removeClass("traversed");
+        });
+  
+        $(".prevbtn1").click(function() {
+          $(".step2").removeClass("active");
+          $(".step1").removeClass("traversed");
+        });
       
   });
   }
